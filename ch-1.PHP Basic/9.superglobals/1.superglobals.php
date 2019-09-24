49918cd3a7d2c50ec783bc6ae2d35312cee2e328
Md. Abutaleb <sumon.pdk.bd@gmail.com>
2019-09-24 14:43:03 +0600

    understanding super global in php

---
+++ ch-1.PHP Basic/9.superglobals/1.superglobals.php
@@ -0,0 +1,14 @@
+<?php
+//header("HTTP/1.1 404 Not Found");
+//print_r(header("Location: http://google.com"));
+echo "<pre>";
+////print_r($argv);
+//echo $_SERVER['REQUEST_TIME_FLOAT'] . "</br>";
+print_r($_SERVER);
+//echo "</pre>";
+
+
+
+
+echo $_SERVER['HTTP_USER_AGENT'] . "\n\n";
+
